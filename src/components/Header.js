f039aa25af704656c05e7215c57c16e5858930eb
RuhaimHakeem <ruhaim860@gmail.com>
2022-10-11 21:23:16 +0530

    Day-1 first commit

---
+++ src/components/Header.js
@@ -0,0 +1,63 @@
+import Image from "next/image";
+import {
+  MenuIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
+
+const Header = () => {
+  return (
+    <header>
+      {/* Top Nav */}
+      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0 ">
+          <Image
+            src="https://links.papareact.com/f90"
+            width={150}
+            height={40}
+            objectFit="contain"
+            className="cursor-pointer"
+          />
+        </div>
+
+        {/* Search Bar */}
+        <div className="hidden sm:flex items-center rounded-md flex-grow cursor-pointer h-10 bg-yellow-400 hover:bg-yellow-500">
+          <input
+            type="text"
+            className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4"
+          />
+          <SearchIcon className="h-12 p-4" />
+        </div>
+
+        {/* Right */}
+
+        <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+          <div className="link ">
+            <p>Hello Ruhaim Hakeem</p>
+            <p className="font-extrabold md:text-sm">Account & Lists</p>
+          </div>
+
+          <div className="link">
+            <p>Returns</p>
+            <p className="font-extrabold md:text-sm">& Orders</p>
+          </div>
+
+          <div className="link flex relative items-center">
+            <span className="absolute top-0 right-0 rounded-full md:right-10 h-4 w-4 bg-yellow-400 text-center text-black font-bold">
+              0
+            </span>
+            <ShoppingCartIcon className="h-10" />
+            <p className=" hidden md:inline font-extrabold md:text-sm mt-2">
+              Basket
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* Bottom Nav */}
+      <div></div>
+    </header>
+  );
+};
+
+export default Header;
